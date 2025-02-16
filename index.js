1a48c722c7762ed179c0e4378e101327d03b7e3c
Nitin Baghel <mailtonitin1024@gmail.com>
2025-02-16 18:43:50 +0530

    Comments added to the javascript file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// Setting the initial values for game 
+
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 var level = 0;
 var playStart = false;
+
+// function to detect the keypress to start the game
 
 $(document).keypress(function () {
   if (!playStart) {
@@ -12,8 +16,10 @@ $(document).keypress(function () {
   }
 });
 
+// function to generate the random game-sequence 
+
 function nextSequence() {
-  userClickedPattern = [];
+  userClickedPattern = [];   /*To reset the value of user-clicked-pattern ,everytime the right sequence is clicked by the user */
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChoosenColor = buttonColors[randomNumber];
@@ -26,6 +32,9 @@ function nextSequence() {
   level++;
   $("#level-title").text("Level " + level);
 }
+
+
+/* Added eventlistener to the buttons so every time the button is clicked it is triggerd */
 $(".btn").click(function () {
   var userChoosenColor = $(this).attr("id");
   console.log(userChoosenColor);
@@ -36,11 +45,13 @@ $(".btn").click(function () {
   checkAnswer(userClickedPattern.length - 1);
 });
 
+/* Added sounds to the button clicked */
 function playSound(name) {
   var audio = new Audio("./sounds/" + name + ".mp3");
   audio.play();
 }
 
+/* Added animation on the buttons pressed */
 function animatePress(currentColor) {
   $("." + currentColor).addClass("pressed");
 
@@ -49,6 +60,8 @@ function animatePress(currentColor) {
   }, 100);
 }
 
+/* function to check the wheather userclicked pattern is right or not */
+
 function checkAnswer(currentLevel) {
   if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
     if (userClickedPattern.length === gamePattern.length) {
@@ -56,6 +69,7 @@ function checkAnswer(currentLevel) {
         nextSequence();
       }, 1000);
     }
+
   } else {
     wrongAnswer();
     $("body").addClass("game-over");
@@ -66,12 +80,13 @@ function checkAnswer(currentLevel) {
     startOver();
   }
 }
-
+/* Audio for the wrong Answer */
 function wrongAnswer() {
   var audio1 = new Audio("./sounds/wrong.mp3");
   audio1.play();
 }
 
+/* Reset */
 function startOver() {
   level = 0;
   gamePattern = [];
